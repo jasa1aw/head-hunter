eabdc534e04966a95ffa4301ab1ffe7cce4e4012
jasa1aw <erakrut05@gmail.com>
2023-12-09 00:33:49 +0600

    complete component 'Autocomplite' and added

--- src/app/create-resume/page.jsx
+++ src/app/create-resume/page.jsx
@@ -3,10 +3,22 @@ import Header from '@/components/header'
 import Input from '@/components/input'
 import { END_POINT } from '@/config/end-point'
 import axios from 'axios'
+import AutoCompliteSelect from '@/components/AutoCompliteSelect'
+import { useEffect, useState } from 'react'
+
 export default function CreateResume() {
-  axios.get(`${END_POINT}/api/region/cities`).then(res => {
-    console.log(res);
-  })
+  const [cities, setCities] = useState([])
+  useEffect(() => {
+      console.log('didMount');
+      axios.get(`${END_POINT}/api/region/cities`).then(res => {
+        setCities(res.data);
+      })
+  }, [])
+
+  console.log('rerender');
+  const onSelect = (data) =>{
+    console.log('onselect', data)
+  }
 
   return (
     <main>
@@ -18,7 +30,7 @@ export default function CreateResume() {
             <Input placeholder={'Введите имя'} type='text' label="Имя" size="fieldset-md"/>
             <Input placeholder={'Введите фамилию'} type='text' label="Фамилия" size="fieldset-md"/>
             <Input placeholder={'+7'} type='text' label="Мобильный телефон" size="fieldset-md"/>
-            <Input placeholder={''} type='text' label="Город проживание" size="fieldset-md"/>
+            <AutoCompliteSelect placeholder={''} type='text' label="Город проживание" size="fieldset-md" items={cities} onSelect={onSelect}/>
         </div>
     </main>
   )
