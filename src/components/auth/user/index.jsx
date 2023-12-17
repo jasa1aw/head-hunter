15f3273381d2f8c1250d657e51de43779d98f3af
jasa1aw <erakrut05@gmail.com>
2023-12-17 19:52:24 +0600

    connecting 'React-Redux' and using it in registration. Logic of registration with 'react-redux' is realised

---
+++ src/components/auth/user/index.jsx
@@ -0,0 +1,74 @@
+'use client';
+import {useEffect, useState} from 'react';
+import { useRouter } from 'next/navigation';
+import { useDispatch, useSelector} from 'react-redux';
+import {authorize, sendVerificationEmail, VerifyCode} from '@/app/store/slices/authSlice';
+
+export default function UserLogin () {
+    const router = useRouter()
+    const isAuth = useSelector((state) => state.auth.isAuth)
+    const dispatch = useDispatch();
+    const [step, setStep] = useState(1);
+    const [email, setEmail] = useState("");
+    const [code, setCode] = useState()
+    const [time, setTime] = useState(119);
+
+
+    const sendVerifyEmail = () => {
+        dispatch(sendVerificationEmail(email));
+        setStep(2);
+    }
+
+    useEffect(() => {
+        let interval;
+        if(step === 2){
+            interval = setInterval(() => {
+                if(time !== 0) setTime(time => time - 1)
+            }, 1000);
+        }else if(interval){
+            clearInterval(interval)
+        }
+    }, [step])
+
+    const min = parseInt(time / 60);
+    const sec = time % 60;
+
+    const verifyCodeFunc = () => {
+        dispatch(VerifyCode(email, code));
+    }
+
+    useEffect(() => {
+        if(isAuth) router.push("/resumes")
+    },[isAuth])
+
+
+    return(
+        <section className="login-page">
+            {isAuth ? 'True' : 'False'}
+            {step == 1 && <div className="card">
+                <h1>Поиск работы</h1>
+                <form>
+                    <input className="input" placeholder="Электронная почта или телефон" value={email} onChange={(e) => setEmail(e.target.value)}/>
+                    <button className="button button-primary" onClick={sendVerifyEmail}>Продолжить</button>
+                </form>
+            </div>}
+            {step == 1 &&<div className="card">
+                <h1>Поиск сотрудников</h1>
+                <p>Размещение вакансий и доступ к базе резюме</p>
+                <button className="button button-primary-bordered">Я ищу сотрудников</button>
+            </div>}
+            {step == 2 && <div className="card">
+                <h1>Отправили код на ....</h1>
+                <p>
+                    Напишите его, чтобы подтвердить, что это вы, а не кто-то другой входить в личный кабинет 
+                </p>
+                <form>
+                    <input className="input" placeholder="Введите код" value={code} onChange={(e) => setCode(e.target.value)}/>
+                    <p>Повторить можно через {min}:{sec}</p>
+                    <button className="button button-primary" type='button' onClick={verifyCodeFunc}>Подтвердить</button>
+                    <button className="button button-primary-bordered"  onClick={()=> setStep(1)}>Назад</button>
+                </form>
+            </div>}
+        </section>
+    )
+}
--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,6 +1,10 @@
 'use client'
 import Link from 'next/link';
+import {useSelector, useDispatch} from 'react-redux';
+import { logOut } from '@/app/store/slices/authSlice';
 export default function Header () {
+    const isAuth = useSelector((state) => state.auth.isAuth);
+    const dispatch = useDispatch()
     return(
         <header className="header">
             <div className="container">
@@ -20,9 +24,12 @@ export default function Header () {
                         <Link href={'/create-resume'} className="header-button header-button-green">
                             Создать резюме
                         </Link>
-                        <Link href={'/login'} className="header-button">
+                        {!isAuth && <Link href={'/login'} className="header-button">
                             Войти
-                        </Link>
+                        </Link>}
+                        {isAuth && <a className="header-button" onClick={() => dispatch(logOut())}>
+                            Выйти
+                        </a>}
                     </div>
                 </div>
             </div>
