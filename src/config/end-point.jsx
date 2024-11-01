a194cc7a9575d6467487cd14009c7fc7f54dbe27
jasa1aw <erakrut05@gmail.com>
2024-11-02 00:16:23 +0500

    fix selectDate

--- src/config/end-point.jsx
+++ src/config/end-point.jsx
@@ -1,2 +1,2 @@
-export const END_POINT = "http://134.209.104.133:3000";
-// export const END_POINT = "http://localhost:3002";
+// export const END_POINT = "http://134.209.104.133:3000";
+export const END_POINT = "http://localhost:3002";
